a21b0d844ce50923a82c15016cf14280c8646f97
bentrueman <bentrueman@192.168.0.109>
2017-11-20 20:32:52 -0700

    finished deck

--- src/fiveDollarBlackJack/Deck.java
+++ src/fiveDollarBlackJack/Deck.java
@@ -1,15 +1,25 @@
 package fiveDollarBlackJack;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Deck {
-	private char[] deck = new char[52];
-	private int index;
+	private Character[] deck = 
+		{'A', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 
+		 'A', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 
+		 'A', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 
+		 'A', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'}; 
+	private int index = 1;
 	
 	public char drawOneCard() {
-		
+		return deck[index++];
 	}
 	
-	public void shuffle() {
-		
+	public void Shuffle() {
+		List<Character> list = Arrays.asList(deck);
+		Collections.shuffle(list);
+		list.toArray(deck);
+//		System.out.println(Arrays.toString(deck));
 	}
-	
 }
