53b29aaa7652ad0ea67e83d9aab18c1c78117fe3
bentrueman <bentrueman@192.168.0.109>
2017-11-20 20:40:51 -0700

    test commit unfinished

--- src/fiveDollarBlackJack/Deck.java
+++ src/fiveDollarBlackJack/Deck.java
@@ -5,11 +5,13 @@ import java.util.Collections;
 import java.util.List;
 
 public class Deck {
-	private Character[] deck = 
-		{'A', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 
-		 'A', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 
-		 'A', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 
-		 'A', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'}; 
+//	private Character[] deck = 
+//		{'A', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 
+//		 'A', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 
+//		 'A', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 
+//		 'A', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'}; 
+	private Character[][] deck = {{'A', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'}, {'♠', '♥', '♦', '♣'}};
+	
 	private int index = 1;
 	
 	public char drawOneCard() {
@@ -17,9 +19,10 @@ public class Deck {
 	}
 	
 	public void Shuffle() {
-		List<Character> list = Arrays.asList(deck);
-		Collections.shuffle(list);
-		list.toArray(deck);
+		
+//		List<Character> list = Arrays.asList(deck);
+//		Collections.shuffle(list);
+//		list.toArray(deck);
 //		System.out.println(Arrays.toString(deck));
 	}
 }
