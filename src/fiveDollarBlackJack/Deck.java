b9b08ce2e047edcdbb5b181a0098c3f3511640d9
bentrueman <bentrueman@192.168.0.109>
2017-11-24 00:02:48 -0700

    Suite added with shuffle

--- src/fiveDollarBlackJack/Deck.java
+++ src/fiveDollarBlackJack/Deck.java
@@ -3,26 +3,42 @@ package fiveDollarBlackJack;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Deck {
 //	private Character[] deck = 
-//		{'A', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 
-//		 'A', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 
-//		 'A', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 
-//		 'A', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'}; 
-	private Character[][] deck = {{'A', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'}, {'♠', '♥', '♦', '♣'}};
+//		{'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 
+//		 'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 
+//		 'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 
+//		 'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'}; 
+	private Character[][] deck = {
+		{'A', '♠'},{'2', '♠'},{'3', '♠'},{'4', '♠'},{'5', '♠'},{'6', '♠'},{'7', '♠'},{'8', '♠'},{'9', '♠'},{'T', '♠'},
+		{'J', '♠'},{'Q', '♠'},{'K', '♠'},{'A', '♥'},{'2', '♥'},{'3', '♥'},{'4', '♥'},{'5', '♥'},{'6', '♥'},{'7', '♥'},
+		{'8', '♥'},{'9', '♥'},{'T', '♥'},{'J', '♥'},{'Q', '♥'},{'K', '♥'},{'A', '♦'},{'2', '♦'},{'3', '♦'},{'4', '♦'},
+		{'5', '♦'},{'6', '♦'},{'7', '♦'},{'8', '♦'},{'9', '♦'},{'T', '♦'},{'J', '♦'},{'Q', '♦'},{'K', '♦'},{'A', '♣'},
+		{'2', '♣'},{'3', '♣'},{'4', '♣'},{'5', '♣'},{'6', '♣'},{'7', '♣'},{'8', '♣'},{'9', '♣'},{'T', '♣'},{'J', '♣'},
+		{'Q', '♣'},{'K', '♣'}};
+
+	private int index;
 	
-	private int index = 1;
-	
-	public char drawOneCard() {
-		return deck[index++];
-	}
+	public String drawOneCard() {
+		return Arrays.toString(deck[index++]);
+	}	
+
+	//	public Character[] drawOneCard() {
+//		return deck[index++];
+//	}
 	
 	public void Shuffle() {
-		
-//		List<Character> list = Arrays.asList(deck);
-//		Collections.shuffle(list);
-//		list.toArray(deck);
+		index = 1;
+		List<Character[]> list = new ArrayList<Character[]>();		
+	    list.addAll(Arrays.asList(deck));
+	    Collections.shuffle(list);
+	    list.toArray(deck);
+	    System.out.println(Arrays.deepToString(deck));
+//		List<Character> list = Arrays.asList(deck); // convert array to list
+//		Collections.shuffle(list); // shuffle list
+//		list.toArray(deck); // convert list back to array
 //		System.out.println(Arrays.toString(deck));
 	}
 }
--- src/fiveDollarBlackJack/FiveDollarBlackJackApp.java
+++ src/fiveDollarBlackJack/FiveDollarBlackJackApp.java
@@ -1,7 +1,13 @@
 package fiveDollarBlackJack;
 
+import java.util.Arrays;
+
 public class FiveDollarBlackJackApp {
+	
 	public static void main(String[] args) {
 		
+		Deck myDeck = new Deck();
+		myDeck.Shuffle();
+		System.out.println(myDeck.drawOneCard());
 	}
 }
