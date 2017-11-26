e69479ca19c3f7c000586fe19e8cc9e2f3ffcabb
bentrueman <bentrueman@192.168.0.109>
2017-11-25 18:27:18 -0700

    more functions added to print current deck, getCards and draw cards

--- src/fiveDollarBlackJack/Deck.java
+++ src/fiveDollarBlackJack/Deck.java
@@ -11,34 +11,43 @@ public class Deck {
 //		 'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 
 //		 'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 
 //		 'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'}; 
-	private Character[][] deck = {
-		{'A', '♠'},{'2', '♠'},{'3', '♠'},{'4', '♠'},{'5', '♠'},{'6', '♠'},{'7', '♠'},{'8', '♠'},{'9', '♠'},{'T', '♠'},
-		{'J', '♠'},{'Q', '♠'},{'K', '♠'},{'A', '♥'},{'2', '♥'},{'3', '♥'},{'4', '♥'},{'5', '♥'},{'6', '♥'},{'7', '♥'},
-		{'8', '♥'},{'9', '♥'},{'T', '♥'},{'J', '♥'},{'Q', '♥'},{'K', '♥'},{'A', '♦'},{'2', '♦'},{'3', '♦'},{'4', '♦'},
-		{'5', '♦'},{'6', '♦'},{'7', '♦'},{'8', '♦'},{'9', '♦'},{'T', '♦'},{'J', '♦'},{'Q', '♦'},{'K', '♦'},{'A', '♣'},
-		{'2', '♣'},{'3', '♣'},{'4', '♣'},{'5', '♣'},{'6', '♣'},{'7', '♣'},{'8', '♣'},{'9', '♣'},{'T', '♣'},{'J', '♣'},
-		{'Q', '♣'},{'K', '♣'}};
+private Character[][] deck = {
+	{'A', '♠'},{'2', '♠'},{'3', '♠'},{'4', '♠'},{'5', '♠'},{'6', '♠'},{'7', '♠'},{'8', '♠'},{'9', '♠'},{'T', '♠'},
+	{'J', '♠'},{'Q', '♠'},{'K', '♠'},{'A', '♥'},{'2', '♥'},{'3', '♥'},{'4', '♥'},{'5', '♥'},{'6', '♥'},{'7', '♥'},
+	{'8', '♥'},{'9', '♥'},{'T', '♥'},{'J', '♥'},{'Q', '♥'},{'K', '♥'},{'A', '♦'},{'2', '♦'},{'3', '♦'},{'4', '♦'},
+	{'5', '♦'},{'6', '♦'},{'7', '♦'},{'8', '♦'},{'9', '♦'},{'T', '♦'},{'J', '♦'},{'Q', '♦'},{'K', '♦'},{'A', '♣'},
+	{'2', '♣'},{'3', '♣'},{'4', '♣'},{'5', '♣'},{'6', '♣'},{'7', '♣'},{'8', '♣'},{'9', '♣'},{'T', '♣'},{'J', '♣'},
+	{'Q', '♣'},{'K', '♣'}};
 
-	private int index;
+private int index;
 	
-	public String drawOneCard() {
-		return Arrays.toString(deck[index++]);
-	}	
+//	public String drawOneCard() {
+//		return Arrays.toString(deck[index++]);
+//	}	
 
-	//	public Character[] drawOneCard() {
-//		return deck[index++];
-//	}
-	
-	public void Shuffle() {
-		index = 1;
-		List<Character[]> list = new ArrayList<Character[]>();		
-	    list.addAll(Arrays.asList(deck));
-	    Collections.shuffle(list);
-	    list.toArray(deck);
-	    System.out.println(Arrays.deepToString(deck));
+public String drawOneCard() {
+	return deck[index][0] + "" + deck[index++][1]; // return card as String of number + suit
+}
+
+public void Shuffle() {
+	index = 1;
+	List<Character[]> list = new ArrayList<Character[]>();
+    list.addAll(Arrays.asList(deck));
+    Collections.shuffle(list);
+    list.toArray(deck);
+    //System.out.println(Arrays.deepToString(deck)); // print out shuffled deck of cards
+}
+
+public String getCard(int i) {
+		return deck[i][0] + "" + deck[i][1]; // return chosen card as String of number + suit
+}
+
+public void getDeck() {
+		System.out.println(Arrays.deepToString(deck)); // print out current deck of cards
+}
+    
 //		List<Character> list = Arrays.asList(deck); // convert array to list
 //		Collections.shuffle(list); // shuffle list
 //		list.toArray(deck); // convert list back to array
 //		System.out.println(Arrays.toString(deck));
-	}
 }
--- src/fiveDollarBlackJack/FiveDollarBlackJackApp.java
+++ src/fiveDollarBlackJack/FiveDollarBlackJackApp.java
@@ -1,13 +1,21 @@
 package fiveDollarBlackJack;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
 
 public class FiveDollarBlackJackApp {
 	
 	public static void main(String[] args) {
 		
 		Deck myDeck = new Deck();
+		myDeck.getDeck();
+		System.out.println(myDeck.getCard(0));
 		myDeck.Shuffle();
+		myDeck.getDeck();
+		System.out.println(myDeck.getCard(0));
+		System.out.println(myDeck.drawOneCard());
 		System.out.println(myDeck.drawOneCard());
 	}
 }
