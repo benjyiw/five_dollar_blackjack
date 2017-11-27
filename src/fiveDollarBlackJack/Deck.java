0c295189533bf692db4571128f1d52672f6240bb
PlayerOneTrent <PlayerOneTrent@144.35.80.51>
2017-11-26 22:08:45 -0700

    Nov 26 Progress 2

--- src/fiveDollarBlackJack/Deck.java
+++ src/fiveDollarBlackJack/Deck.java
@@ -1,15 +1,53 @@
 package fiveDollarBlackJack;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Deck {
-	private char[] deck = new char[52];
-	private int index;
+//	private Character[] deck = 
+//		{'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 
+//		 'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 
+//		 'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 
+//		 'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'}; 
+private static Character[][] deck = {
+	{'A', '♠'},{'2', '♠'},{'3', '♠'},{'4', '♠'},{'5', '♠'},{'6', '♠'},{'7', '♠'},{'8', '♠'},{'9', '♠'},{'T', '♠'},
+	{'J', '♠'},{'Q', '♠'},{'K', '♠'},{'A', '♥'},{'2', '♥'},{'3', '♥'},{'4', '♥'},{'5', '♥'},{'6', '♥'},{'7', '♥'},
+	{'8', '♥'},{'9', '♥'},{'T', '♥'},{'J', '♥'},{'Q', '♥'},{'K', '♥'},{'A', '♦'},{'2', '♦'},{'3', '♦'},{'4', '♦'},
+	{'5', '♦'},{'6', '♦'},{'7', '♦'},{'8', '♦'},{'9', '♦'},{'T', '♦'},{'J', '♦'},{'Q', '♦'},{'K', '♦'},{'A', '♣'},
+	{'2', '♣'},{'3', '♣'},{'4', '♣'},{'5', '♣'},{'6', '♣'},{'7', '♣'},{'8', '♣'},{'9', '♣'},{'T', '♣'},{'J', '♣'},
+	{'Q', '♣'},{'K', '♣'}};
+
+public static int index;
 	
-	public char drawOneCard() {
-		
-	}
-	
-	public void shuffle() {
-		
-	}
-	
+//	public String drawOneCard() {
+//		return Arrays.toString(deck[index++]);
+//	}	
+
+public String drawOneCard() {
+	return deck[index][0] + "" + deck[index++][1]; // return card as String of number + suit
+}
+
+public void Shuffle() {
+	index = 1;
+	List<Character[]> list = new ArrayList<Character[]>();
+    list.addAll(Arrays.asList(deck));
+    Collections.shuffle(list);
+    list.toArray(deck);
+    //System.out.println(Arrays.deepToString(deck)); // print out shuffled deck of cards
+}
+
+public String getCard(int i) {
+		return deck[i][0] + "" + deck[i][1]; // return chosen card as String of number + suit
+}
+
+public void getDeck() {
+		System.out.println(Arrays.deepToString(deck)); // print out current deck of cards
+}
+    
+//		List<Character> list = Arrays.asList(deck); // convert array to list
+//		Collections.shuffle(list); // shuffle list
+//		list.toArray(deck); // convert list back to array
+//		System.out.println(Arrays.toString(deck));
 }
--- src/fiveDollarBlackJack/FiveDollarBlackJackApp.java
+++ src/fiveDollarBlackJack/FiveDollarBlackJackApp.java
@@ -1,11 +1,24 @@
+/*****************************************
+* Project: $5 Blackjack
+* Group Members: Trent Greguhn, Benjamin Wesemann, Benjamin Trueman
+* Class Author : Trent Greguhn
+* Date : 11/19/2017
+* Assignment: A08 – Patterns
+******************************************/
+
 package fiveDollarBlackJack;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class FiveDollarBlackJackApp {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int playerBet = 0;
+		char playerChoice;
+		ArrayList<String> playerCards = new ArrayList<String>();
+		ArrayList<String> dealerCards = new ArrayList<String>();
+		Deck myDeck = new Deck();
 		
 		System.out.println("Welcome to $5 Blackjack!");
 		System.out.print("How much money do you have?: $");
@@ -27,10 +40,47 @@ public class FiveDollarBlackJackApp {
 		
 		System.out.println("Let's start!");
 		System.out.println("Dealer is shuffling...");
-//		Deck.shuffle();
-		sleep();
-		System.out.println("Dealer has");
+		myDeck.Shuffle();
+		sleep(2000);
+		System.out.println("Dealer is drawing...");
+		sleep(2000);
+		String dealerCardOne = myDeck.drawOneCard();
+		String dealerCardTwo = myDeck.drawOneCard();
+		dealerCards.add(dealerCardOne);
+		dealerCards.add(dealerCardTwo);
+		System.out.printf("Dealer has %s [*]%n", dealerCardOne);
+		System.out.println("Dealer is dealing your hand...");
+		sleep(3000);
+		String playerCardOne = myDeck.drawOneCard();
+		String playerCardTwo = myDeck.drawOneCard();
+		playerCards.add(playerCardOne);
+		playerCards.add(playerCardTwo);
+		System.out.printf("You have: %s %s%n", playerCardOne, playerCardTwo);
+		System.out.print("Would you like to Hit (H) or Stay (S)?: ");
+		playerChoice = input.next(".").charAt(0);
+		do {
+		switch(playerChoice) {
+		case 'h':
+		case 'H': System.out.println("Drawing...");
+				  sleep(2000);
+				  String userCardThree = myDeck.drawOneCard();
+				  playerCards.add(userCardThree);
+				  System.out.printf("You have: %s %s %s%n",
+						  playerCardOne, playerCardTwo, userCardThree);
+				  /*if (Hand.getTotal(playerCards) > 21) {
+					  
+				  }*/
+				  break;
+		case 's':
+		case 'S':
+		default: System.out.print("Please Hit (H) or Stay (S): ");
+				 playerChoice = input.next(".").charAt(0);
+		}
+		}while(playerChoice == 'H' || playerChoice == 'S');
 		
+		
+		
+		input.close();
 	}
 	
 /*	public static void userBet(int playerBet) {
@@ -42,11 +92,12 @@ public class FiveDollarBlackJackApp {
 		input.close();
 	}*/
 	
-	public static void sleep() {
+	public static void sleep(int milliseconds) {
 		try {
-		    Thread.sleep(3000);                 //1000 milliseconds is one second.
+		    Thread.sleep(milliseconds);                 //1000 milliseconds is one second.
 		} catch(InterruptedException ex) {
 		    Thread.currentThread().interrupt();
 		}
 	}
+	
 }
