47af9d337fd8a0fcec916177894c3c1677db7224
Ben Wesemann <bwesemann@connexionpoint.com>
2017-11-27 20:49:10 -0700

    adding everything for hand class. not sure if we need to add more.

--- src/fiveDollarBlackJack/Hand.java
+++ src/fiveDollarBlackJack/Hand.java
@@ -3,23 +3,45 @@ package fiveDollarBlackJack;
 import java.util.ArrayList;
 
 public class Hand {
-	private ArrayList<Character> hand = new ArrayList<Character>();
-	private int total;
-	
-	public void addCard(char card) {
-		
-	}
-	
-	public char[] getOrigHand() {
-		
-	}
-	
-	public String[] getCleanHand() {
-		
+	private ArrayList<Character[]> hand = new ArrayList<>();
+	private int[] total = {0, 0};
+	private boolean hasAce = false;
+
+	public void addCard(Character[] card) {
+        hand.add(card);
+
+        switch (card[0]) {
+            case 'A':
+                if (hasAce) {
+                    total[0] += 1;
+                    total[1] += 1;
+                } else {
+                    hasAce = true;
+                    total[0] += 11;
+                    total[1] += 1;
+                }
+                break;
+            case 'T': case 'J': case 'Q': case 'K':
+                total[0] += 10;
+                total[1] += 10;
+                break;
+            default:
+                total[0] += card[0] - '0';
+                total[1] += card[0] - '0';
+                break;
+        }
+    }
+
+	public Character[][] getHand() {
+		return hand.toArray(new Character[][]{new Character[hand.size()]});
 	}
 	
 	public int[] getTotal() {
-		
+        return total;
 	}
-	
+
+	public boolean checkHasAce() {
+	    return hasAce;
+    }
+
 }
